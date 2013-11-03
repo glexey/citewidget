b96462f8e95ea1b77c38642e9d5cfcc512adad09
aagaidiu <alexey.a.gaidiukov@intel.com>
2013-11-03 11:33:08 -0800

    LangDBManager testAddQuote passes

--- src/org/glexey/citewidget/LangDBManager.java
+++ src/org/glexey/citewidget/LangDBManager.java
@@ -4,8 +4,10 @@
 package org.glexey.citewidget;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import org.glexey.citewidget.Cite;
+import org.glexey.citewidget.LangDB.LangDBException;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -20,19 +22,18 @@ public class LangDBManager {
 	public static final int historySize = 20; 
 	
 	private String[] languageList;
-	//private Context ctx;
+	private Context ctx;
 	
 	/**
 	 * @param ctx 
 	 * 
 	 */
 	public LangDBManager(Context ctx, int resID) {
-		//this.ctx = ctx;
+		this.ctx = ctx;
 
 		// read the list of languages from the resources
-		Resources res = ctx.getResources();
+		Resources res = this.ctx.getResources();
 		languageList = res.getStringArray(resID);
-
 	}
 
 	/**
@@ -43,13 +44,48 @@ public class LangDBManager {
 		return stubCite;
 	}
 
-	/**
+	/** Get quote from specified language
 	 * @param string
 	 * @return
 	 */
 	public Cite getNextQuote(String lang) {
-		// TODO Auto-generated method stub
-		return null;
+		Cite cite = getNextQuote(lang, "web");
+		if (cite != null) return cite;
+		return getNextQuote(lang, "fix");
+	}
+
+	/** Get next quote from DB specified by "lang.src".
+	 *  If dictionary is empty, returns null.
+	 * @param lang
+	 * @param src
+	 * @return
+	 */
+	public Cite getNextQuote(String lang, String src) {
+		LangDB db = new LangDB(ctx, lang + "." + src);
+		LangDB hist = new LangDB(ctx, "history");
+		Cite cite;
+		try {
+			cite = db.shift();
+			if (cite != null)
+				hist.append(cite);
+			return cite;
+		} catch (LangDBException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/** Add a quote of given language to "web" dictionary
+	 * @param string
+	 * @param cite1
+	 */
+	public void addQuote(String lang, Cite cite) {
+		LangDB db = new LangDB(ctx, lang + ".web");
+		try {
+			db.append(cite);
+		} catch (LangDBException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -67,17 +103,29 @@ public class LangDBManager {
 	 * dictionary, which will be appended on each getNextQuote() call.
 	 */
 	public void initFromScratch() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * @param string
-	 * @param cite1
-	 */
-	public void addQuote(String lang, Cite cite) {
-		// TODO Auto-generated method stub
-		
+		for (int i=0; i<languageList.length; i++) {
+			String lang = languageList[i];
+			LangDB fix_db = new LangDB(ctx, lang + ".fix");
+			LangDB web_db = new LangDB(ctx, lang + ".web");
+			int fixResId = ctx.getResources().getIdentifier(
+					"CiteArr" + lang.toUpperCase(Locale.US), "id", ctx.getPackageName());
+			try {
+				fix_db.deleteDB();
+				fix_db.createDB();
+				web_db.deleteDB();
+				web_db.createDB();
+				if (fixResId != 0) fix_db.updateFromResource(fixResId);
+			} catch (LangDBException e) {
+				e.printStackTrace(); // STUB
+			}
+		}
+		LangDB hist_db = new LangDB(ctx, "history");
+		try {
+			hist_db.deleteDB();
+			hist_db.createDB();
+		} catch (LangDBException e) {
+			e.printStackTrace(); // STUB
+		}
 	}
 
 	/**
--- tests/src/org/glexey/citewidget/test/ValidateLangDBManager.java
+++ tests/src/org/glexey/citewidget/test/ValidateLangDBManager.java
@@ -19,7 +19,6 @@ import android.test.InstrumentationTestCase;
 public class ValidateLangDBManager extends InstrumentationTestCase {
 
 	private Context tst_ctx;
-	//private RenamingDelegatingContext target_ctx;
 
 	/* (non-Javadoc)
 	 * @see junit.framework.TestCase#setUp()
@@ -28,11 +27,6 @@ public class ValidateLangDBManager extends InstrumentationTestCase {
 		
 		// context of the AndroidTestCase
 		tst_ctx = getInstrumentation().getContext();
-
-		// For ******** testing, have to use the context of target application,
-		// otherwise file creation will fail due to lack of access.
-		// Use renaming context not to accidentally corrupt the application files.
-		//target_ctx = new RenamingDelegatingContext(getInstrumentation().getTargetContext(), "test_");
 
 		super.setUp();
 	}
@@ -45,11 +39,20 @@ public class ValidateLangDBManager extends InstrumentationTestCase {
 	}
 
 	// The main functionality of LangDBManager class is to give us quotes
-	// let's test that it always gives us some quote, even if a stub
+	// let's test that it always gives us some quote, even if a stub.
 	public void testGetNextQuoteBasic() {
 		LangDBManager reader = new LangDBManager(tst_ctx, R.array.languages);
 		Cite cite = reader.getNextQuote();
 		assertNotNull(cite);
+	}
+		
+	// getNextQuote("lang", "src") should return null if corresponding DB is empty
+	public void testGetNextQuoteLangSrcBasic() {
+		LangDBManager reader = new LangDBManager(tst_ctx, R.array.languages);
+		// Create a set of 2 dictionaries per language, e.g.: "ru.fix", "ru.web"
+		reader.initFromScratch();
+		Cite cite = reader.getNextQuote("ru", "web");
+		assertNull(cite);
 	}
 	
 	// LangDBManager should get the list of languages from resources upon
@@ -78,8 +81,8 @@ public class ValidateLangDBManager extends InstrumentationTestCase {
 		// Create a set of 2 dictionaries per language, e.g.: "ru.fix", "ru.web"
 		reader.initFromScratch();
 		Cite cite1 = new Cite("Test quote");
-		reader.addQuote("ru.web", cite1);
-		Cite cite2 = reader.getNextQuote("ru.web");
+		reader.addQuote("ru", cite1);
+		Cite cite2 = reader.getNextQuote("ru", "web");
 		assertTrue(cite1.equals(cite2));
 	}
 	
